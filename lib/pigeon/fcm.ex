075fbed69fa8df40305f12d6ba5df2a5eac0e693
Nick <nick.duitz@9elements.com>
2025-10-07 09:42:38 +0200

    more resilient requests, check if connected before

--- lib/pigeon/fcm.ex
+++ lib/pigeon/fcm.ex
@@ -141,23 +141,23 @@ defmodule Pigeon.FCM do
 
   @impl Pigeon.Adapter
   def handle_push(notification, state) do
-    %{config: config, queue: queue, socket: socket} = state
-    headers = Configurable.push_headers(config, notification, [])
-    payload = Configurable.push_payload(config, notification, [])
-    method = "POST"
-    path = "/v1/projects/#{config.project_id}/messages:send"
+    %{socket: socket, config: config} = state
 
-    {:ok, socket, ref} =
-      Mint.HTTP.request(socket, method, path, headers, payload)
+    Mint.HTTP.open?(socket)
+    |> if do
+      do_request!(notification, state)
+    else
+      case connect_socket(config) do
+        {:ok, socket} ->
+          Configurable.schedule_ping(config)
+          state = %{state | socket: socket}
 
-    new_q = RequestQueue.add(queue, ref, notification)
+          do_request!(notification, state)
 
-    state =
-      state
-      |> Map.put(:socket, socket)
-      |> Map.put(:queue, new_q)
-
-    {:noreply, state}
+        {:error, reason} ->
+          {:stop, reason}
+      end
+    end
   end
 
   @impl Pigeon.Adapter
@@ -200,6 +200,26 @@ defmodule Pigeon.FCM do
         |> Map.put(:response, Error.parse(error))
         |> process_on_response()
     end
+  end
+
+  defp do_request!(notification, state) do
+    %{config: config, queue: queue, socket: socket} = state
+    headers = Configurable.push_headers(config, notification, [])
+    payload = Configurable.push_payload(config, notification, [])
+    method = "POST"
+    path = "/v1/projects/#{config.project_id}/messages:send"
+
+    {:ok, socket, ref} =
+      Mint.HTTP.request(socket, method, path, headers, payload)
+
+    new_q = RequestQueue.add(queue, ref, notification)
+
+    state =
+      state
+      |> Map.put(:socket, socket)
+      |> Map.put(:queue, new_q)
+
+    {:noreply, state}
   end
 
   @spec connect_socket(Config.t()) :: {:ok, Mint.HTTP2.t()} | {:error, term()}
